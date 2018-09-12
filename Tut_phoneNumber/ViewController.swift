6c810d136545a76924292213a640ec039bb8f77e
Eray Alparslan <erayalparslan@yahoo.com>
2018-09-12 15:19:49 +0300

    iOS Notları

--- Tut_phoneNumber/ViewController.swift
+++ Tut_phoneNumber/ViewController.swift
@@ -7,19 +7,74 @@
 //
 
 import UIKit
+import Contacts
+import ContactsUI
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, CNContactViewControllerDelegate {
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var phoneLabel: UILabel!
+    @IBOutlet weak var mailLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        let name  = nameLabel.text!
+        let phone = phoneLabel.text!
+        let mail  = mailLabel.text!
+        
+        saveContact(contactName: name, contactPhone: phone, contactMail: mail)
     }
+    
+    func saveContact(contactName name: String, contactPhone phone: String, contactMail mail: String) {
+        
+        //CNMutableContact instance i yarat
+        let newContact = CNMutableContact()
+        
+        //E-posta adresini belirtelim
+        let homeEmail = CNLabeledValue(label:"E-mail:", value: "\(mail)" as NSString)
+        
+        //E-posta adresini contact bilgisine kayıt edelim.
+        //İstersek başka e-mail adresleri de ekleyelim ',' ile ayırabiliriz
+        newContact.emailAddresses = [homeEmail]
+        newContact.phoneNumbers = [CNLabeledValue(
+            label: "Phone Number:",
+            value:CNPhoneNumber(stringValue:"\(phone.digits)"))]
+        
+        //Kişinin profil resmini belirlemek isterseniz aşağıdaki kodu kullanabilirsiniz
+        //newContact.imageData = UIImagePNGRepresentation(UIImage(named: "pictureName")!)
+        
+        //İsim bilgisini kaydedelim
+        newContact.givenName = name
+        newContact.familyName = "iOS Notları"
+        
+        //Present etmek üzere ContactViewController oluşturalım ve bilgileri bunun içine gönderelim
+        let contactVC = CNContactViewController(forUnknownContact: newContact)
+        contactVC.contactStore = CNContactStore()
+        
+        contactVC.delegate = self
+        contactVC.allowsActions = false
+        
+        //Present edelim ve uygulamadan cihaza geçiş yapalım
+        let navigationController = UINavigationController(rootViewController: contactVC)
+        self.present(navigationController, animated: true, completion: nil)
+        
+    }
+    
+    //Kaydettikten sonra uygulamaya geri dön
+    func contactViewController(_ viewController: CNContactViewController, didCompleteWith contact: CNContact?) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+}
 
-
+extension String {
+    var digits: String {
+        return components(separatedBy: CharacterSet.decimalDigits.inverted)
+            .joined()
+    }
 }
 
